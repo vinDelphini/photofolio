835905715a927ce4770c40501f824c4bcca7f97f
vinclairvoyant <venkatesh.bolisetti@clairvoyantsoft.com>
2022-08-24 14:50:39 +0530

    first commit

---
+++ commands.sh
@@ -0,0 +1,35 @@
+[Unit]
+Description=gunicorn daemon
+Requires=gunicorn.socket
+After=network.target
+
+[Service]
+User=ubuntu
+Group=www-data
+WorkingDirectory=/home/ubuntu/photofolio
+ExecStart=/home/ubuntu/photofolio/venv/bin/gunicorn \
+          --access-logfile - \
+          --workers 3 \
+          --bind unix:/run/gunicorn.sock \
+          photofolio.wsgi:application
+
+[Install]
+WantedBy=multi-user.target
+
+
+
+server {
+    listen 80;
+    server_name 13.58.104.201;
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    
+    location /static/ {
+        root /home/ubuntu/photofolio;
+    }
+
+    location / {
+        include proxy_params;
+        proxy_pass http://unix:/run/gunicorn.sock;
+    }
+}
