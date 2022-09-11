939832cd6b303b0ffdac9bac029ae260ace56891
vinclairvoyant <venkatesh.bolisetti@clairvoyantsoft.com>
2022-09-11 17:21:39 +0530

    BASE_DIR

--- photofolio/settings.py
+++ photofolio/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
